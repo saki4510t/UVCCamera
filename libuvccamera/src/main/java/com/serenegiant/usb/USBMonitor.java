6466855673673b6a601444a15b63073830932f42
chenff <chenf_1992@126.com>
2019-03-19 13:57:03 +0800

    fixed crash bug on android 9.0

--- libuvccamera/src/main/java/com/serenegiant/usb/USBMonitor.java
+++ libuvccamera/src/main/java/com/serenegiant/usb/USBMonitor.java
@@ -982,6 +982,8 @@ public final class USBMonitor {
 			}
 			mBusNum = busnum;
 			mDevNum = devnum;
+
+			try {
 //			if (DEBUG) {
 				if (mConnection != null) {
 					final int desc = mConnection.getFileDescriptor();
@@ -991,6 +993,9 @@ public final class USBMonitor {
 					Log.e(TAG, "could not connect to device " + name);
 				}
 //			}
+			} catch (Throwable e){
+				Log.e(TAG, "UsbControlBlock Throwable:" + e.toString());
+			}
 		}
 
 		/**
