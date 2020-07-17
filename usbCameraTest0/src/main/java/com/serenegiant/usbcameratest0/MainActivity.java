647786b22479b3ed9cef8e61c4e7c518f545b0e3
iosdevlog <iosdevlog@iosdevlog.com>
2020-07-17 20:17:48 +0800

    h264 file preview

--- usbCameraTest0/src/main/java/com/serenegiant/usbcameratest0/MainActivity.java
+++ usbCameraTest0/src/main/java/com/serenegiant/usbcameratest0/MainActivity.java
@@ -24,6 +24,8 @@
 package com.serenegiant.usbcameratest0;
 
 import android.hardware.usb.UsbDevice;
+import android.media.MediaCodec;
+import android.media.MediaFormat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Surface;
@@ -31,6 +33,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -42,6 +45,12 @@ import com.serenegiant.usb.USBMonitor.OnDeviceConnectListener;
 import com.serenegiant.usb.USBMonitor.UsbControlBlock;
 import com.serenegiant.usb.UVCCamera;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 
 public class MainActivity extends BaseActivity implements CameraDialog.CameraDialogParent {
@@ -58,6 +67,27 @@ public class MainActivity extends BaseActivity implements CameraDialog.CameraDia
 	private Surface mPreviewSurface;
 	private boolean isActive, isPreview;
 
+
+
+	private String h264Path = "/mnt/sdcard/720pq.h264";
+	private File h264File = new File(h264Path);
+	private InputStream is = null;
+	private FileInputStream fs = null;
+
+	private SurfaceView mSurfaceView;
+	private Button mReadButton;
+	private MediaCodec mCodec;
+
+	Thread readFileThread;
+	boolean isInit = false;
+
+	// Video Constants
+	private final static String MIME_TYPE = "video/avc"; // H.264 Advanced Video
+	private final static int VIDEO_WIDTH = 848;
+	private final static int VIDEO_HEIGHT = 480;
+	private final static int TIME_INTERNAL = 30;
+	private final static int HEAD_OFFSET = 512;
+
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -69,7 +99,199 @@ public class MainActivity extends BaseActivity implements CameraDialog.CameraDia
 		mUVCCameraView.getHolder().addCallback(mSurfaceViewCallback);
 
 		mUSBMonitor = new USBMonitor(this, mOnDeviceConnectListener);
+
+		initH264();
 	}
+
+	private void initH264() {
+		mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView1);
+		mReadButton = (Button) findViewById(R.id.btn_readfile);
+		mReadButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (h264File.exists()) {
+					if (!isInit) {
+						initDecoder();
+						isInit = true;
+					}
+
+					readFileThread = new Thread(readFile);
+					readFileThread.start();
+				} else {
+					Toast.makeText(getApplicationContext(),
+							"H264 file not found", Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+	}
+
+	public void initDecoder() {
+
+		try {
+			mCodec = MediaCodec.createDecoderByType(MIME_TYPE);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		MediaFormat mediaFormat = MediaFormat.createVideoFormat(MIME_TYPE,
+				VIDEO_WIDTH, VIDEO_HEIGHT);
+		mCodec.configure(mediaFormat, mSurfaceView.getHolder().getSurface(),
+				null, 0);
+		mCodec.start();
+	}
+
+	int mCount = 0;
+
+	public boolean onFrame(byte[] buf, int offset, int length) {
+		Log.e("Media", "onFrame start");
+		Log.e("Media", "onFrame Thread:" + Thread.currentThread().getId());
+		// Get input buffer index
+		ByteBuffer[] inputBuffers = mCodec.getInputBuffers();
+		int inputBufferIndex = mCodec.dequeueInputBuffer(100);
+
+		Log.e("Media", "onFrame index:" + inputBufferIndex);
+		if (inputBufferIndex >= 0) {
+			ByteBuffer inputBuffer = inputBuffers[inputBufferIndex];
+			inputBuffer.clear();
+			inputBuffer.put(buf, offset, length);
+			mCodec.queueInputBuffer(inputBufferIndex, 0, length, mCount
+					* TIME_INTERNAL, 0);
+			mCount++;
+		} else {
+			return false;
+		}
+
+		// Get output buffer index
+		MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+		int outputBufferIndex = mCodec.dequeueOutputBuffer(bufferInfo, 100);
+		while (outputBufferIndex >= 0) {
+			mCodec.releaseOutputBuffer(outputBufferIndex, true);
+			outputBufferIndex = mCodec.dequeueOutputBuffer(bufferInfo, 0);
+		}
+		Log.e("Media", "onFrame end");
+		return true;
+	}
+
+	/**
+	 * Find H264 frame head
+	 *
+	 * @param buffer
+	 * @param len
+	 * @return the offset of frame head, return 0 if can not find one
+	 */
+	static int findHead(byte[] buffer, int len) {
+		int i;
+		for (i = HEAD_OFFSET; i < len; i++) {
+			if (checkHead(buffer, i))
+				break;
+		}
+		if (i == len)
+			return 0;
+		if (i == HEAD_OFFSET)
+			return 0;
+		return i;
+	}
+
+	/**
+	 * Check if is H264 frame head
+	 *
+	 * @param buffer
+	 * @param offset
+	 * @return whether the src buffer is frame head
+	 */
+	static boolean checkHead(byte[] buffer, int offset) {
+		// 00 00 00 01
+		if (buffer[offset] == 0 && buffer[offset + 1] == 0
+				&& buffer[offset + 2] == 0 && buffer[3] == 1)
+			return true;
+		// 00 00 01
+		if (buffer[offset] == 0 && buffer[offset + 1] == 0
+				&& buffer[offset + 2] == 1)
+			return true;
+		return false;
+	}
+
+	Runnable readFile = new Runnable() {
+
+		@Override
+		public void run() {
+			int h264Read = 0;
+			int frameOffset = 0;
+			byte[] buffer = new byte[100000];
+			byte[] framebuffer = new byte[200000];
+			boolean readFlag = true;
+			try {
+				fs = new FileInputStream(h264File);
+				is = new BufferedInputStream(fs);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			while (!Thread.interrupted() && readFlag) {
+				try {
+					int length = is.available();
+					if (length > 0) {
+						// Read file and fill buffer
+						int count = is.read(buffer);
+						Log.i("count", "" + count);
+						h264Read += count;
+						Log.d("Read", "count:" + count + " h264Read:"
+								+ h264Read);
+						// Fill frameBuffer
+						if (frameOffset + count < 200000) {
+							System.arraycopy(buffer, 0, framebuffer,
+									frameOffset, count);
+							frameOffset += count;
+						} else {
+							frameOffset = 0;
+							System.arraycopy(buffer, 0, framebuffer,
+									frameOffset, count);
+							frameOffset += count;
+						}
+
+						// Find H264 head
+						int offset = findHead(framebuffer, frameOffset);
+						Log.i("find head", " Head:" + offset);
+						while (offset > 0) {
+							if (checkHead(framebuffer, 0)) {
+								// Fill decoder
+								boolean flag = onFrame(framebuffer, 0, offset);
+								if (flag) {
+									byte[] temp = framebuffer;
+									framebuffer = new byte[200000];
+									System.arraycopy(temp, offset, framebuffer,
+											0, frameOffset - offset);
+									frameOffset -= offset;
+									Log.e("Check", "is Head:" + offset);
+									// Continue finding head
+									offset = findHead(framebuffer, frameOffset);
+								}
+							} else {
+
+								offset = 0;
+							}
+
+						}
+						Log.d("loop", "end loop");
+					} else {
+						h264Read = 0;
+						frameOffset = 0;
+						readFlag = false;
+						// Start a new thread
+						readFileThread = new Thread(readFile);
+						readFileThread.start();
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				try {
+					Thread.sleep(TIME_INTERNAL);
+				} catch (InterruptedException e) {
+
+				}
+			}
+		}
+	};
 
 	@Override
 	protected void onStart() {
@@ -110,6 +332,7 @@ public class MainActivity extends BaseActivity implements CameraDialog.CameraDia
 		mUVCCameraView = null;
 		mCameraButton = null;
 		super.onDestroy();
+		readFileThread.interrupt();
 	}
 
 	private final OnClickListener mOnClickListener = new OnClickListener() {
